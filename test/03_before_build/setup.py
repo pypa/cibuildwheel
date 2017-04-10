278661df77b1cf23938b2495287c78800cf36237
Joe Rickerby <joerick@mac.com>
2017-04-10 22:11:10 +0100

    remove redundant line

--- test/03_before_build/setup.py
+++ test/03_before_build/setup.py
@@ -3,7 +3,6 @@ import sys
 
 # here we assert that the Python version as written to version.txt in the CIBW_BEFORE_BUILD step
 # is the same one as is currently running.
-sys.stderr.write('sys.argv \n' + str(sys.argv))
 if sys.argv[-1] != '--name':
     with open('version.txt') as f:
         stored_version = f.read()
