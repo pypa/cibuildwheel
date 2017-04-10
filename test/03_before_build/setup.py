7a2e951f488c6ca15a1e54f577843c1211c5b59e
Joe Rickerby <joerick@mac.com>
2017-04-10 22:36:06 +0100

    Change version file path to absolute on Windows

--- test/03_before_build/setup.py
+++ test/03_before_build/setup.py
@@ -4,7 +4,8 @@ import sys
 # here we assert that the Python version as written to version.txt in the CIBW_BEFORE_BUILD step
 # is the same one as is currently running.
 if sys.argv[-1] != '--name':
-    with open('pythonversion.txt') as f:
+    version_file = 'c:\\pythonversion.txt' if os.platform == 'win32' else 'pythonversion.txt'
+    with open(version_file) as f:
         stored_version = f.read()
     print('stored_version', stored_version)
     print('sys.version', sys.version)
