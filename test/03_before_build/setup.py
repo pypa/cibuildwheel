cf7f2e18ced19f05a4cf7354f9553c85cd667c47
Joe Rickerby <joerick@mac.com>
2017-04-11 23:23:50 +0100

    Style

--- test/03_before_build/setup.py
+++ test/03_before_build/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, Extension
 import sys
 
-# here we assert that the Python version as written to version.txt in the CIBW_BEFORE_BUILD step
-# is the same one as is currently running.
 if sys.argv[-1] != '--name':
+    # assert that the Python version as written to version.txt in the CIBW_BEFORE_BUILD step
+    # is the same one as is currently running.
     version_file = 'c:\\pythonversion.txt' if sys.platform == 'win32' else '/tmp/pythonversion.txt'
     with open(version_file) as f:
         stored_version = f.read()
