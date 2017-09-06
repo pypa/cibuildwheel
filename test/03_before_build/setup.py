bade2da061815771e44a01bb2242a08841469bba
Joe Rickerby <joerick@mac.com>
2017-09-06 22:02:10 +0100

    Add test for before_build executing in a shell

--- test/03_before_build/setup.py
+++ test/03_before_build/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, Extension
-import sys
+import sys, os
 
 if sys.argv[-1] != '--name':
-    # assert that the Python version as written to version.txt in the CIBW_BEFORE_BUILD step
+    # assert that the Python version as written to pythonversion.txt in the CIBW_BEFORE_BUILD step
     # is the same one as is currently running.
     version_file = 'c:\\pythonversion.txt' if sys.platform == 'win32' else '/tmp/pythonversion.txt'
     with open(version_file) as f:
@@ -10,6 +10,20 @@ if sys.argv[-1] != '--name':
     print('stored_version', stored_version)
     print('sys.version', sys.version)
     assert stored_version == sys.version
+
+    # check that the executable also was written
+    executable_file = 'c:\\pythonexecutable.txt' if sys.platform == 'win32' else '/tmp/pythonexecutable.txt'
+    with open(executable_file) as f:
+        stored_executable = f.read()
+
+    stored_executable_real = os.path.realpath(stored_executable)
+    sys_executable_real = os.path.realpath(sys.executable)
+    
+    print('stored_executable_real', stored_executable_real)
+    print('sys_executable_real', sys_executable_real)
+    assert stored_executable_real == sys_executable_real
+
+    print('PATH', os.environ['PATH'])
 
 setup(
     name="spam",
