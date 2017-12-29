9013a7783f397f4254f534ff16b78daa2d6307b0
Yannick Jadoul <Yannick.Jadoul@ai.vub.ac.be>
2017-12-29 13:33:11 +0100

    Adding a check for Linux to test 06_docker_images's check for the '/dockcross' directory

--- test/06_docker_images/setup.py
+++ test/06_docker_images/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, Extension
 if sys.argv[-1] != '--name':
     # check that we're running in the correct docker image as specified in the
     # environment options CIBW_MANYLINUX1_*_IMAGE
-    if not os.path.exists('/dockcross'):
+    if 'linux' in sys.platform and not os.path.exists('/dockcross'):
         raise Exception('/dockcross directory not found. Is this test running in the correct docker image?')
 
 setup(
