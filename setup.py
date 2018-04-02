7f51b1d8e7355905b001cb93002e43ac59f743b8
Joe Rickerby <joerick@mac.com>
2018-04-02 15:49:39 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='cibuildwheel',
-    version='0.7.0',
+    version='0.7.1',
     install_requires=['bashlex'],
     description="Build Python wheels on CI with minimal configuration.",
     long_description='For readme please see http://github.com/joerick/cibuildwheel',
