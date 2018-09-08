c3f64ff08d36fe55d90801c7b45fcc5fffdcd09c
Joe Rickerby <joerick@mac.com>
2018-09-08 17:46:47 +0100

    Restructure code to make error handling stricter (and comment sections)

--- cibuildwheel/linux.py
+++ cibuildwheel/linux.py
@@ -148,53 +148,60 @@ def build(project_dir, package_name, output_dir, test_command, test_requires, be
             stdin=subprocess.PIPE,
             universal_newlines=True,
         )
+        try:
+            # Copy the project into the Docker container
+            #
+            # It will take a bit for docker to get the container up and
+            # running.  Keep trying to copy in the project directory until
+            # it succeeds.  There is a timeout to avoid spinning forever.
+            timeout = 60
+            start = monotonic.monotonic()
+            while True:
+                time.sleep(1)
+                command = [
+                    'docker',
+                    'cp',
+                    '{}/.'.format(os.path.abspath(project_dir)),
+                    '{}:/project'.format(container_name),
+                ]
+                print('docker command: {}'.format(command))
+                if subprocess.call(command) == 0:
+                    break
 
-        # It will take a bit for docker to get the container up and
-        # running.  Keep trying to copy in the project directory until
-        # it succeeds.  There is a timeout to avoid spinning forever.
+                if monotonic.monotonic() - start > timeout:
+                    raise DockerRunTimeoutError(
+                        'Unable to successfully copy project directory'
+                        ' within {} seconds'.format(timeout)
+                    )
 
-        timeout = 180
-        start = monotonic.monotonic()
-        while True:
-            time.sleep(1)
+            # Run the bash script
+            try:
+                docker_process.communicate(bash_script)
+            except KeyboardInterrupt:
+                docker_process.kill()
+                docker_process.wait()
+                raise
+
+            if docker_process.returncode != 0:
+                exit(1)
+
+            # Build done. Pull the wheels from the container.
             command = [
                 'docker',
                 'cp',
-                '{}/.'.format(os.path.abspath(project_dir)),
-                '{}:/project'.format(container_name),
+                '{}:/output/.'.format(container_name),
+                os.path.abspath(output_dir),
             ]
             print('docker command: {}'.format(command))
-            if subprocess.call(command) == 0:
-                break
+            subprocess.check_call(command)
+        finally:
+            # Cleanup: remove the docker container and its volumes
+            command = [
+                'docker',
+                'rm',
+                '--force',
+                '-v', container_name,
+            ]
+            print('docker command: {}'.format(command))
+            subprocess.check_call(command)
 
-            if monotonic.monotonic() - start > timeout:
-                raise DockerRunTimeoutError(
-                    'Unable to successfully copy project directory'
-                    ' within {} seconds'.format(timeout)
-                )
-
-        try:
-            docker_process.communicate(bash_script)
-        except KeyboardInterrupt:
-            docker_process.kill()
-            docker_process.wait()
-
-        command = [
-            'docker',
-            'cp',
-            '{}:/output/.'.format(container_name),
-            os.path.abspath(output_dir),
-        ]
-        print('docker command: {}'.format(command))
-        subprocess.check_call(command)
-
-        command = [
-            'docker',
-            'rm',
-            '-v', container_name,
-        ]
-        print('docker command: {}'.format(command))
-        subprocess.check_call(command)
-
-        if docker_process.returncode != 0:
-            exit(1)
