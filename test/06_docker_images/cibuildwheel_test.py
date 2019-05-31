235567c394b49d1356cb8ba58d1984efe1fc5d0d
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-05-31 20:31:58 +0200

    Updating dockcross images in docker images test, including the new dockcross/manylinux2010-x64

--- test/06_docker_images/cibuildwheel_test.py
+++ test/06_docker_images/cibuildwheel_test.py
@@ -8,13 +8,12 @@ def test():
         pytest.skip('the docker test is only relevant to the linux build')
 
     utils.cibuildwheel_run(project_dir, add_env={
-        'CIBW_MANYLINUX1_X86_64_IMAGE': 'dockcross/manylinux-x64',
-        'CIBW_MANYLINUX1_I686_IMAGE': 'dockcross/manylinux-x86',
-        'CIBW_SKIP': '*-manylinux2010_*',  # No dockcross image for manylinux2010 (yet)
+        'CIBW_MANYLINUX1_X86_64_IMAGE': 'dockcross/manylinux1-x64',
+        'CIBW_MANYLINUX1_I686_IMAGE': 'dockcross/manylinux1-x86',
+        'CIBW_MANYLINUX2010_X86_64_IMAGE': 'dockcross/manylinux2010-x64',
     })
 
     # also check that we got the right wheels built
-    expected_wheels = [w for w in utils.expected_wheels('spam', '0.1.0')
-                       if '-manylinux2010' not in w]
+    expected_wheels = [w for w in utils.expected_wheels('spam', '0.1.0')]
     actual_wheels = os.listdir('wheelhouse')
     assert set(actual_wheels) == set(expected_wheels)
