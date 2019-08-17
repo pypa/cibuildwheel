27517cd8a085371c210e6b88141aae289603fd91
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-08-18 00:42:35 +0200

    Updating test 06_docker_images to use new CIBW_MANYLINUX_*_IMAGE options

--- test/06_docker_images/cibuildwheel_test.py
+++ test/06_docker_images/cibuildwheel_test.py
@@ -8,11 +8,13 @@ def test():
         pytest.skip('the docker test is only relevant to the linux build')
 
     utils.cibuildwheel_run(project_dir, add_env={
-        'CIBW_MANYLINUX1_X86_64_IMAGE': 'dockcross/manylinux-x64',
-        'CIBW_MANYLINUX1_I686_IMAGE': 'dockcross/manylinux-x86',
+        'CIBW_MANYLINUX_X86_64_IMAGE': 'dockcross/manylinux2010-x64',
+        'CIBW_MANYLINUX_I686_IMAGE': 'dockcross/manylinux1-x86',
+        'CIBW_BEFORE_BUILD': '/opt/python/cp36-cp36m/bin/pip install -U auditwheel',  # Currently necessary on dockcross images to get auditwheel 2.1 supporting AUDITWHEEL_PLAT
+        'CIBW_ENVIRONMENT': 'AUDITWHEEL_PLAT=`if [ $(uname -i) == "x86_64" ]; then echo "manylinux2010_x86_64"; else echo "manylinux1_i686"; fi`',
     })
 
     # also check that we got the right wheels built
-    expected_wheels = utils.expected_wheels('spam', '0.1.0')
+    expected_wheels = utils.expected_wheels('spam', '0.1.0', manylinux_versions={'1_x86_64', '2010_x86_64', '1_i686'})
     actual_wheels = os.listdir('wheelhouse')
     assert set(actual_wheels) == set(expected_wheels)
