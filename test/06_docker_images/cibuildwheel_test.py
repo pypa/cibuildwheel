ea6221ca2e719f2180046ee58ef2eb4160212a91
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-10-16 11:50:47 +0200

    Fixing macOS build identifier and skipping 06_docker_images test for Python 3.8 until dockcross images are ready

--- test/06_docker_images/cibuildwheel_test.py
+++ test/06_docker_images/cibuildwheel_test.py
@@ -12,10 +12,11 @@ def test():
         'CIBW_MANYLINUX_I686_IMAGE': 'dockcross/manylinux1-x86',
         'CIBW_BEFORE_BUILD': '/opt/python/cp36-cp36m/bin/pip install -U auditwheel',  # Currently necessary on dockcross images to get auditwheel 2.1 supporting AUDITWHEEL_PLAT
         'CIBW_ENVIRONMENT': 'AUDITWHEEL_PLAT=`if [ $(uname -i) == "x86_64" ]; then echo "manylinux2010_x86_64"; else echo "manylinux1_i686"; fi`',
+        'CIBW_SKIP': 'cp38-*',
     })
 
     # also check that we got the right wheels built
     expected_wheels = [w for w in utils.expected_wheels('spam', '0.1.0')
-                       if '-manylinux2010_i686' not in w]
+                       if '-manylinux2010_i686' not in w and 'cp38-' not in w]
     actual_wheels = os.listdir('wheelhouse')
     assert set(actual_wheels) == set(expected_wheels)
