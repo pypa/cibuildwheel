1b970744bfb65183e8d2806750329642ad8eb029
mayeut <mayeut@users.noreply.github.com>
2019-10-26 13:45:09 +0200

    Install python with official installer if an official install is not found
    Vendor-In pep514tools

---
+++ cibuildwheel/_vendored/pep514tools/__init__.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Steve Dower
+# All rights reserved.
+#
+# Distributed under the terms of the MIT License
+#-------------------------------------------------------------------------
+
+__author__ = 'Steve Dower <steve.dower@python.org>'
+__version__ = '0.1.0'
+
+from .environment import findall, find, findone
