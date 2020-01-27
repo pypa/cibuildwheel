3472e7de06ab75888d231407da31cfb61493e8b6
Joe Rickerby <joerick@mac.com>
2020-01-27 21:45:55 +0000

    Remove old test projects

--- test/02_test/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, Extension
-
-setup(
-    name="spam",
-    ext_modules=[Extension("spam", sources=["spam.c"])],
-    extras_require={"test": ["nose"]},
-    version="0.1.0",
-)
