c33fa2ada06286e0855b73ddfe4f3fb0a9666bd9
Joe Rickerby <joerick@mac.com>
2020-03-07 12:21:31 +0000

    Add test for boolean ops on windows

--- test/02_test/cibuildwheel_test.py
+++ test/02_test/cibuildwheel_test.py
@@ -32,7 +32,7 @@ def test_extras_require():
         # the 'false ||' bit is to ensure this command runs in a shell on
         # mac/linux.
         'CIBW_TEST_COMMAND': 'false || nosetests {project}/test',
-        'CIBW_TEST_COMMAND_WINDOWS': 'nosetests {project}/test',
+        'CIBW_TEST_COMMAND_WINDOWS': 'COLOR 00 || nosetests {project}/test',
     })
 
     # also check that we got the right wheels
