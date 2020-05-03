bf882b065db3952297cfa85538c1e204440ea293
Joe Rickerby <joerick@mac.com>
2020-05-03 14:18:57 +0100

    Output test timings

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     subprocess.check_call([sys.executable, '-m', 'pytest', 'unit_test'])
 
     # run the integration tests
-    subprocess.check_call([sys.executable, '-m', 'pytest', 'test'])
+    subprocess.check_call([sys.executable, '-m', 'pytest', '--durations', '0', 'test'])
