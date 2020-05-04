a2fff6164da6efb0c5e2f1d77a78413439d4cfb5
Yannick Jadoul <yannick.jadoul@belgacom.net>
2020-05-04 15:49:31 +0200

    Also try to build C++14/C++17 wheels for PyPy 3.6 with default MSVC

--- test/10_cpp_standards/cibuildwheel_test.py
+++ test/10_cpp_standards/cibuildwheel_test.py
@@ -40,11 +40,10 @@ def test_cpp17():
 
     # Python and PyPy 2.7 use the `register` keyword which is forbidden in the C++17 standard
     # The manylinux1 docker image does not have a compiler which supports C++11
-    # Python 3.5 and PyPy 3.6 are compiled with MSVC 10, which does not support C++17
     if os.environ.get('APPVEYOR_BUILD_WORKER_IMAGE', '') == 'Visual Studio 2015':
         pytest.skip('Visual Studio 2015 does not support C++17')
 
-    add_env = {'CIBW_SKIP': 'cp27-win* pp27-win32 pp36-win32', 'CIBW_ENVIRONMENT': 'STANDARD=17'}
+    add_env = {'CIBW_SKIP': 'cp27-win* pp27-win32', 'CIBW_ENVIRONMENT': 'STANDARD=17'}
 
     if utils.platform == 'macos':
         add_env['MACOSX_DEPLOYMENT_TARGET'] = '10.13'
@@ -52,8 +51,7 @@ def test_cpp17():
     actual_wheels = utils.cibuildwheel_run(project_dir, add_env=add_env)
     expected_wheels = [w for w in utils.expected_wheels('spam', '0.1.0', macosx_deployment_target='10.13')
                        if 'cp27-cp27m-win' not in w
-                       and 'pp27-pypy_73-win32' not in w
-                       and 'pp36-pypy36_pp73-win32' not in w]
+                       and 'pp27-pypy_73-win32' not in w]
 
     assert set(actual_wheels) == set(expected_wheels)
 
