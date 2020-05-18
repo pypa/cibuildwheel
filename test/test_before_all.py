2e15fdddfd9e44c07d573d18a7e12e914036dbde
Grzegorz Bokota <bokota+github@gmail.com>
2020-05-18 10:42:13 +0200

    change to new test engine

--- test/test_before_all.py
+++ test/test_before_all.py
@@ -1,10 +1,25 @@
 import os
+import textwrap
 
-import utils
+from . import utils
+from . import test_projects
+
+project_with_before_build_asserts = test_projects.new_c_project(
+    setup_py_add=textwrap.dedent(r'''
+        # assert that the Python version as written to text_info.txt in the CIBW_BEFORE_ALL step
+        # is the same one as is currently running.
+        with open("text_info.txt") as f:
+            stored_text = f.read()
+
+        print("## stored text: " + stored_text)
+        assert stored_text == "sample text 123"
+    ''')
+)
 
 
-def test():
-    project_dir = os.path.dirname(__file__)
+def test(tmp_path):
+    project_dir = tmp_path / 'project'
+    project_with_before_build_asserts.generate(project_dir)
 
     with open(os.path.join(project_dir, "text_info.txt"), mode='w') as ff:
         print("dummy text", file=ff)
