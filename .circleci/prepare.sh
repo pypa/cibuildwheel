4302090f080b2dfc709210f41750ce66e06f530b
Joe Rickerby <joerick@mac.com>
2020-06-23 10:22:01 +0100

    Uninstall poetry

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -3,7 +3,8 @@ set -o xtrace
 
 $PYTHON --version
 $PYTHON -m pip --version
-$PYTHON -m pip install -q --user --ignore-installed virtualenv==20.0.23
+sudo $PYTHON -m pip uninstall -y poetry
+$PYTHON -m pip install --user --ignore-installed virtualenv==20.0.21
 $PYTHON -m virtualenv -p $PYTHON venv
 venv/bin/python -m pip install -r requirements-dev.txt
 venv/bin/python -m pip freeze
