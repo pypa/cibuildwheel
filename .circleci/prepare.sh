c68d50a07387272f6f31962930842421e99648e6
Joe Rickerby <joerick@mac.com>
2020-06-23 10:13:54 +0100

    Lock virtualenv version

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -1,6 +1,9 @@
+set -o errexit
+set -o xtrace
+
 $PYTHON --version
 $PYTHON -m pip --version
-$PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
+$PYTHON -m pip install -q --user --ignore-installed virtualenv==20.0.23
 $PYTHON -m virtualenv -p $PYTHON venv
 venv/bin/python -m pip install -r requirements-dev.txt
 venv/bin/python -m pip freeze
