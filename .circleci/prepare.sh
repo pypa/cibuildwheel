947059bbf453079bbc571755b0f0d329fde34612
Joe Rickerby <joerick@mac.com>
2020-06-23 10:27:23 +0100

    Don't try to install virtualenv

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -3,8 +3,6 @@ set -o xtrace
 
 $PYTHON --version
 $PYTHON -m pip --version
-sudo $PYTHON -m pip uninstall -y poetry
-$PYTHON -m pip install --user --ignore-installed virtualenv==20.0.21
 $PYTHON -m virtualenv -p $PYTHON venv
 venv/bin/python -m pip install -r requirements-dev.txt
 venv/bin/python -m pip freeze
