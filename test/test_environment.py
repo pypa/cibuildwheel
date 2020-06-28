27d97daf13b6aa04301640d225845344aad17fac
Joe Rickerby <joerick@mac.com>
2020-06-28 11:33:22 +0100

    Don't use redirection syntax in Windows environment. Fix absolute path use for Mac

--- test/test_environment.py
+++ test/test_environment.py
@@ -55,8 +55,8 @@ def test_overridden_path(tmp_path, capfd):
     # mess up PATH, somehow
     with pytest.raises(subprocess.CalledProcessError):
         utils.cibuildwheel_run(project_dir, output_dir=output_dir, add_env={
-            'CIBW_ENVIRONMENT': '''SOMETHING="$(mkdir -p /new_path ; touch /new_path/python ; chmod +x /new_path/python)" PATH="/new_path:$PATH"''',
-            'CIBW_ENVIRONMENT_WINDOWS': '''SOMETHING="$(mkdir new_path && type nul > new_path/python.exe)" PATH="$CD\\new_path;$PATH"''',
+            'CIBW_ENVIRONMENT': '''SOMETHING="$(mkdir -p new_path ; touch new_path/python ; chmod +x new_path/python)" PATH="$(pwd)/new_path:$PATH"''',
+            'CIBW_ENVIRONMENT_WINDOWS': '''SOMETHING="$(mkdir new_path ; fsutil file createnew new_path/python.exe 0)" PATH="$CD\\new_path;$PATH"''',
         })
 
     assert len(os.listdir(output_dir)) == 0
