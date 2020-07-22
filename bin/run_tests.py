83cf785cb1b7cafc2858be4bb11ceaa6d4537fab
Grzegorz Bokota <bokota+github@gmail.com>
2020-07-22 16:20:07 +0200

    extract check version test to separated run

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -18,4 +18,6 @@ if __name__ == '__main__':
 
     subprocess.check_call([sys.executable, '-m', 'pytest', '-x', '--durations', '0', 'test/test_0_basic.py', '-n0'])
     # run the integration tests
-    subprocess.check_call([sys.executable, '-m', 'pytest', '-x', '--durations', '0', 'test', '-n', '2', '--dist=loadfile'])
+    subprocess.check_call([sys.executable, '-m', 'pytest', '-x', '--durations', '0', 'test', '-n', '2', '--dist=loadfile', '--ignore="test/test_0_basic.py"', '--ignore="test/test_dependency_versions.py"'])
+
+    subprocess.check_call([sys.executable, '-m', 'pytest', '-x', '--durations', '0', 'test/test_dependency_versions.py', '-n0'])
