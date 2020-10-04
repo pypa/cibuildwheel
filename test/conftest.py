7c4a1bfe239ba06ffed5502a0b41da2ab9a31717
Grzegorz Bokota <bokota+github@gmail.com>
2020-10-04 13:09:11 +0200

    add missed argument

--- test/conftest.py
+++ test/conftest.py
@@ -7,7 +7,7 @@ import cibuildwheel.windows
 def setup_python(monkeypatch, request):
     try:
         from xdist.plugin import get_xdist_worker_id
-        worker_id = get_xdist_worker_id()
+        worker_id = get_xdist_worker_id(request)
         monkeypatch.setattr(cibuildwheel.windows, "INSTALL_PATH", 'C:\\cibw\\{}'.format(worker_id))
     except ImportError:
         pass
