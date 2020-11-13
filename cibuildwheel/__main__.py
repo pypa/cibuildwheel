8e2381bfb13231ae50ad4fc2776c613452b5fd71
Joe Rickerby <joerick@mac.com>
2020-11-13 17:40:07 +0000

    add some encoding debug code

--- cibuildwheel/__main__.py
+++ cibuildwheel/__main__.py
@@ -91,6 +91,17 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    # TODO remove this encoding stuff
+    import locale
+    import os
+    print(sys.stdout.encoding)
+    print(sys.stdout.isatty())
+    print(locale.getpreferredencoding())
+    print(sys.getfilesystemencoding())
+    print(os.environ["PYTHONIOENCODING"])
+    print(chr(246), chr(9786), chr(9787))
+    # TODO end
+
     detect_obsolete_options()
 
     if args.platform != 'auto':
