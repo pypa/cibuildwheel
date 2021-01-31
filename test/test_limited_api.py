2920e75af649b3b389fed977abfadd91c2082860
Yannick Jadoul <yannick.jadoul@belgacom.net>
2021-01-31 22:37:33 +0100

    Add check on duplicate wheel error

--- test/test_limited_api.py
+++ test/test_limited_api.py
@@ -1,4 +1,7 @@
+import subprocess
 import textwrap
+
+import pytest
 
 from . import test_projects, utils
 
@@ -25,7 +28,7 @@ def test_setup_cfg(tmp_path):
     assert set(actual_wheels) == set(expected_wheels)
 
 
-def test_build_option_env(tmp_path):
+def test_build_option_env(tmp_path, capfd):
     project_dir = tmp_path / 'project'
     test_projects.new_c_project().generate(project_dir)
 
@@ -39,3 +42,17 @@ def test_build_option_env(tmp_path):
     expected_wheels = [w for w in utils.expected_wheels('spam', '0.1.0', limited_api='cp36')
                        if '-pp' not in w]
     assert set(actual_wheels) == set(expected_wheels)
+
+
+def test_duplicate_wheel_error(tmp_path, capfd):
+    project_dir = tmp_path / 'project'
+    limited_api_project.generate(project_dir)
+
+    with pytest.raises(subprocess.CalledProcessError):
+        utils.cibuildwheel_run(project_dir, add_env={
+            'CIBW_BUILD': 'cp36-* cp37-*',
+        })
+
+    captured = capfd.readouterr()
+    assert "already exists in output directory" in captured.err
+    assert "It looks like you are building wheels against Python's limited API" in captured.err
