cad5f0b2f62c63b48237ca2db940d3f45eda5a66
mayeut <mayeut@users.noreply.github.com>
2021-05-24 18:17:39 +0200

    test

--- .circleci/prepare.sh
+++
@@ -1,9 +0,0 @@
-set -o errexit
-set -o xtrace
-
-$PYTHON --version
-$PYTHON -m pip --version
-$PYTHON -m virtualenv -p $PYTHON venv
-venv/bin/python -m pip install -e ".[dev]"
-venv/bin/python -m pip freeze
-venv/bin/python --version
