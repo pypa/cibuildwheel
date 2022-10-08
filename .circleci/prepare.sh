1ee0692f680753d2c2706c5a52e557fe958e3265
Henry Schreiner <henryschreineriii@gmail.com>
2022-10-08 15:58:04 -0400

    Revert "ci: avoid editable installs with old pip"
    This reverts commit 0fe600eea319ee508bc834f95355a6e9404e00b8.

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -5,6 +5,6 @@ set -o xtrace
 $PYTHON --version
 $PYTHON -m pip --version
 $PYTHON -m virtualenv -p "$PYTHON" venv
-venv/bin/python -m pip install ".[dev]"
+venv/bin/python -m pip install -e ".[dev]"
 venv/bin/python -m pip freeze
 venv/bin/python --version
