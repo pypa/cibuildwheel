ac8e4af18ad116d9cdbf034bb174d341860acaac
Henry Schreiner <henryschreineriii@gmail.com>
2023-10-06 18:54:51 -0400

    fix(schema): differing conventions
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- bin/generate_schema.py
+++ bin/generate_schema.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 starter = """
 $id: https://github.com/pypa/cibuildwheel/blob/main/cibuildwheel/resources/cibuildwheel.schema.json
-$schema: http://json-schema.org/draft-07/schema#
+$schema: http://json-schema.org/draft-07/schema
 additionalProperties: false
 description: cibuildwheel's settings.
 type: object
@@ -150,6 +150,9 @@ properties:
 """
 
 schema = yaml.safe_load(starter)
+
+if args.schemastore:
+    schema["$id"] += "#"
 
 string_array = yaml.safe_load(
     """
