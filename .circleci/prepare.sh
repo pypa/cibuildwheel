7085318a16f569248aa302052f55aab6a7b11194
Joe Rickerby <joerick@mac.com>
2023-11-24 12:03:22 +0000

    Use the full resource class names

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -4,7 +4,7 @@ set -o xtrace
 
 python3 --version
 python3 -m pip --version
-python3 -m virtualenv -p "$PYTHON" venv
+python3 -m venv venv
 venv/bin/python -m pip install -e ".[dev]"
 venv/bin/python -m pip freeze
 venv/bin/python --version
