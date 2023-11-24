102914df012a104fcf6b3e2b72f5e98fc955d8e9
Joe Rickerby <joerick@mac.com>
2023-11-24 10:49:19 +0000

    Try the macos ARM runners

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -2,9 +2,9 @@
 set -o errexit
 set -o xtrace
 
-$PYTHON --version
-$PYTHON -m pip --version
-$PYTHON -m virtualenv -p "$PYTHON" venv
+python3 --version
+python3 -m pip --version
+python3 -m virtualenv -p "$PYTHON" venv
 venv/bin/python -m pip install -e ".[dev]"
 venv/bin/python -m pip freeze
 venv/bin/python --version
