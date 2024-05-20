1b354cfa6382745855f4c47b84a151302f56cba8
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-05-20 02:45:05 -0400

    chore: move to hatchling (#1297)

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -4,7 +4,9 @@ set -o xtrace
 
 $PYTHON --version
 $PYTHON -m pip --version
-$PYTHON -m virtualenv -p "$PYTHON" venv
+$PYTHON -m virtualenv --version
+$PYTHON -m virtualenv --no-setuptools --no-wheel -p "$PYTHON" venv
+venv/bin/python -m pip install -U pip
 venv/bin/python -m pip install -e ".[dev]"
 venv/bin/python -m pip freeze
 venv/bin/python --version
