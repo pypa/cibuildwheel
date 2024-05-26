809427edecfb0a34c7938c14db162ba7d86a5726
Hood Chatham <roberthoodchatham@gmail.com>
2024-05-26 14:10:47 -0700

    Add reference to emscripten libc issue

--- test/test_projects/c.py
+++ test/test_projects/c.py
@@ -51,6 +51,7 @@ from setuptools import setup, Extension
 
 libraries = []
 # Emscripten fails if you pass -lc...
+# See: https://github.com/emscripten-core/emscripten/issues/16680
 if sys.platform.startswith('linux') and "emscripten" not in os.environ.get("_PYTHON_HOST_PLATFORM", ""):
     libraries.extend(['m', 'c'])
 
