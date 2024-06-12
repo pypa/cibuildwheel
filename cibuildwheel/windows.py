230a869e9670ca75bd886ab05d74544a0aeaffbe
Mike Foster <i.am.mike.foster@gmail.com>
2024-06-12 20:28:38 +0200

    set VSCMD_ARG_TGT_ARCH based on targetted architecture

--- cibuildwheel/windows.py
+++ cibuildwheel/windows.py
@@ -152,6 +152,12 @@ def setup_setuptools_cross_compile(
     # set the platform name
     map_plat = {"32": "win32", "64": "win-amd64", "ARM64": "win-arm64"}
     plat_name = map_plat[python_configuration.arch]
+
+    # Set environment variable so that setuptools._distutils.get_platform()
+    # identifies the target, not the host
+    vscmd_arg_tgt_arch = {"32": "x86", "64": "x64", "ARM64": "arm64"}
+    env["VSCMD_ARG_TGT_ARCH"] = vscmd_arg_tgt_arch[python_configuration.arch]
+
     # (This file must be default/locale encoding, so we can't pass 'encoding')
     distutils_cfg.write_text(
         textwrap.dedent(
