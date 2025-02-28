8fe9a3b8b4411e732b2a3babbf062c92ac255105
Henry Schreiner <henryschreineriii@gmail.com>
2025-02-28 11:59:39 -0500

    chore: remove PopenBytes & TYPE_CHECKING
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- cibuildwheel/typing.py
+++ cibuildwheel/typing.py
@@ -1,24 +1,17 @@
 from __future__ import annotations
 
 import os
-import subprocess
 import typing
-from typing import Final, Literal, Protocol, Union
+from typing import Final, Literal, Protocol
 
 __all__ = (
     "PLATFORMS",
     "PathOrStr",
     "PlatformName",
-    "PopenBytes",
 )
 
 
-if typing.TYPE_CHECKING:
-    PopenBytes = subprocess.Popen[bytes]
-    PathOrStr = str | os.PathLike[str]
-else:
-    PopenBytes = subprocess.Popen
-    PathOrStr = Union[str, "os.PathLike[str]"]
+PathOrStr = str | os.PathLike[str]
 
 
 PlatformName = Literal["linux", "macos", "windows", "pyodide"]
