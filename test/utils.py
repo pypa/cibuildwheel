0e1a4c8336ba03715085bd4714e337194afb8884
Henry Schreiner <henryschreineriii@gmail.com>
2025-03-19 15:49:27 -0400

    refactor: use listify in tests
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- test/utils.py
+++ test/utils.py
@@ -4,14 +4,15 @@ Utility functions used by the cibuildwheel tests.
 This file is added to the PYTHONPATH in the test runner at bin/run_test.py.
 """
 
+import functools
 import os
 import platform as pm
 import subprocess
 import sys
-from collections.abc import Mapping, Sequence
+from collections.abc import Callable, Generator, Mapping, Sequence
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any, Final
+from typing import Any, Final, ParamSpec, TypeVar
 
 import pytest
 
@@ -151,6 +152,19 @@ def _floor_macosx(*args: str) -> str:
     return max(args, key=lambda x: tuple(map(int, x.split("."))))
 
 
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def _listify(func: Callable[P, Generator[R, None, None]]) -> Callable[P, list[R]]:
+    @functools.wraps(func)
+    def listify_return(*args: P.args, **kwargs: P.kwargs) -> list[R]:
+        return list(func(*args, **kwargs))
+
+    return listify_return
+
+
+@_listify
 def expected_wheels(
     package_name: str,
     package_version: str,
@@ -162,9 +176,9 @@ def expected_wheels(
     include_universal2: bool = False,
     single_python: bool = False,
     single_arch: bool = False,
-) -> list[str]:
+) -> Generator[str, None, None]:
     """
-    Returns a list of expected wheels from a run of cibuildwheel.
+    Returns the expected wheels from a run of cibuildwheel.
     """
     # per PEP 425 (https://www.python.org/dev/peps/pep-0425/), wheel files shall have name of the form
     # {distribution}-{version}(-{build tag})?-{python tag}-{abi tag}-{platform tag}.whl
@@ -240,13 +254,12 @@ def expected_wheels(
             )
         ]
 
-    wheels: list[str] = []
-
     if platform == "pyodide":
         assert len(python_abi_tags) == 1
         python_abi_tag = python_abi_tags[0]
         platform_tag = "pyodide_2024_0_wasm32"
-        return [f"{package_name}-{package_version}-{python_abi_tag}-{platform_tag}.whl"]
+        yield f"{package_name}-{package_version}-{python_abi_tag}-{platform_tag}.whl"
+        return
 
     for python_abi_tag in python_abi_tags:
         platform_tags = []
@@ -317,12 +330,8 @@ def expected_wheels(
             msg = f"Unsupported platform {platform!r}"
             raise Exception(msg)
 
-        wheels.extend(
-            f"{package_name}-{package_version}-{python_abi_tag}-{platform_tag}.whl"
-            for platform_tag in platform_tags
-        )
-
-    return wheels
+        for platform_tag in platform_tags:
+            yield f"{package_name}-{package_version}-{python_abi_tag}-{platform_tag}.whl"
 
 
 def get_macos_version() -> tuple[int, int]:
