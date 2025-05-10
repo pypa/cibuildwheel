9ea0fac9b6647b8a00c322859f912830b669a028
Joe Rickerby <joerick@mac.com>
2025-05-10 17:02:26 +0100

    Add TEST_ENVIRONMENT option and set PYTHONSAFEPATH=1 in the test env
    Closes https://github.com/pypa/cibuildwheel/issues/2358

--- cibuildwheel/platforms/pyodide.py
+++ cibuildwheel/platforms/pyodide.py
@@ -380,6 +380,12 @@ def build(options: Options, tmp_path: Path) -> None:
                 )
                 virtualenv_env["VIRTUAL_ENV"] = str(venv_dir)
 
+                virtualenv_env["PYTHONSAFEPATH"] = "1"
+
+                virtualenv_env = build_options.test_environment.as_dictionary(
+                    prev_environment=virtualenv_env
+                )
+
                 # check that we are using the Python from the virtual environment
                 call("which", "python", env=virtualenv_env)
 
