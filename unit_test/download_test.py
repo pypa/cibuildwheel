f97d5a8e3d6d52fd93190640db5c4bc2a5c01fdd
Joe Rickerby <joerick@mac.com>
2025-05-16 17:15:25 +0100

    Use jsdelivr for Github asset mirroring

--- unit_test/download_test.py
+++ unit_test/download_test.py
@@ -5,21 +5,21 @@ import pytest
 
 from cibuildwheel.util.file import download
 
-DOWNLOAD_URL = "https://badssl.com"
+DOWNLOAD_URL = "https://cdn.jsdelivr.net/gh/pypa/cibuildwheel@v1.6.3/requirements-dev.txt"
 
 
 def test_download(monkeypatch, tmp_path):
     monkeypatch.delenv("SSL_CERT_FILE", raising=False)
     dest = tmp_path / "file.txt"
     download(DOWNLOAD_URL, dest)
-    assert len(dest.read_bytes()) == 11673
+    assert len(dest.read_bytes()) == 134
 
 
 def test_download_good_ssl_cert_file(monkeypatch, tmp_path):
     monkeypatch.setenv("SSL_CERT_FILE", certifi.where())
     dest = tmp_path / "file.txt"
     download(DOWNLOAD_URL, dest)
-    assert len(dest.read_bytes()) == 11673
+    assert len(dest.read_bytes()) == 134
 
 
 def test_download_bad_ssl_cert_file(monkeypatch, tmp_path):
