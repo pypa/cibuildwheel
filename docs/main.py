3cb492b2a7d17f8dc6254aea9d49f648530816b6
Henry Schreiner <henryschreineriii@gmail.com>
2025-05-22 00:18:04 -0400

    docs: color output for docs
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- docs/main.py
+++ docs/main.py
@@ -1,7 +1,11 @@
 import os
+import re
 import subprocess
 import sysconfig
 from typing import Any
+
+import rich.console
+import rich.text
 
 
 def define_env(env: Any) -> None:
@@ -12,5 +16,13 @@ def define_env(env: Any) -> None:
         "Run a subprocess and return the stdout"
         env = os.environ.copy()
         scripts = sysconfig.get_path("scripts")
+        env.pop("NO_COLOR", None)
         env["PATH"] = f"{scripts}{os.pathsep}{env.get('PATH', '')}"
-        return subprocess.run(args, check=True, capture_output=True, text=True, env=env).stdout
+        env["PYTHON_COLORS"] = "1"
+        output = subprocess.run(args, check=True, capture_output=True, text=True, env=env).stdout
+        rich_text = rich.text.Text.from_ansi(output)
+        console = rich.console.Console(record=True)
+        console.print(rich_text)
+        page = console.export_html(inline_styles=True)
+        text = re.search(r"<body.*?>(.*?)</body>", page, re.DOTALL | re.IGNORECASE).group(1)
+        return text.strip()
