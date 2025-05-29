86dafd0028aabba97bc4c2087aa199352ca86d89
Henry Schreiner <henryschreineriii@gmail.com>
2025-05-29 13:25:10 -0400

    chore: use match for stringify_setting
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- cibuildwheel/options.py
+++ cibuildwheel/options.py
@@ -355,28 +355,33 @@ def _stringify_setting(
     setting: SettingValue,
     option_format: OptionFormat | None,
 ) -> str:
-    if isinstance(setting, Mapping):
-        try:
-            if option_format is None:
-                raise OptionFormat.NotSupported
-            return option_format.format_table(setting)
-        except OptionFormat.NotSupported:
-            msg = f"Error converting {setting!r} to a string: this setting doesn't accept a table"
-            raise OptionsReaderError(msg) from None
-
-    if not isinstance(setting, str) and isinstance(setting, Sequence):
-        try:
-            if option_format is None:
-                raise OptionFormat.NotSupported
-            return option_format.format_list(setting)
-        except OptionFormat.NotSupported:
-            msg = f"Error converting {setting!r} to a string: this setting doesn't accept a list"
-            raise OptionsReaderError(msg) from None
-
-    if isinstance(setting, bool | int):
-        return str(setting)
-
-    return setting
+    match setting:
+        case {}:
+            assert isinstance(setting, Mapping)  # MyPy 1.15 doesn't narrow this for us
+            try:
+                if option_format is None:
+                    raise OptionFormat.NotSupported
+                return option_format.format_table(setting)
+            except OptionFormat.NotSupported:
+                msg = (
+                    f"Error converting {setting!r} to a string: this setting doesn't accept a table"
+                )
+                raise OptionsReaderError(msg) from None
+        case bool() | int():
+            return str(setting)
+        case [*_]:
+            try:
+                if option_format is None:
+                    raise OptionFormat.NotSupported
+                return option_format.format_list(setting)
+            except OptionFormat.NotSupported:
+                msg = (
+                    f"Error converting {setting!r} to a string: this setting doesn't accept a list"
+                )
+                raise OptionsReaderError(msg) from None
+        case _:
+            assert isinstance(setting, str)  # MyPy 1.15 doesn't narrow this for us
+            return setting
 
 
 class OptionsReader:
