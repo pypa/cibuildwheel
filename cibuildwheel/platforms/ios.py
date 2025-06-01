2125d7117ef94c79a8df9e7be388c3d210a2eba0
Henry Schreiner <HenrySchreinerIII@gmail.com>
2025-05-31 23:36:02 -0400

    fix: test-sources should use project dir (#2437)
    * fix: test-sources should use project dir
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    * docs: fix docstring
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- cibuildwheel/platforms/ios.py
+++ cibuildwheel/platforms/ios.py
@@ -565,7 +565,7 @@ def build(options: Options, tmp_path: Path) -> None:
                     if build_options.test_sources:
                         copy_test_sources(
                             build_options.test_sources,
-                            build_options.package_dir,
+                            Path.cwd(),
                             testbed_app_path,
                         )
                     else:
