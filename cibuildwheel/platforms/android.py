ec0744cde83f44517a4cfa3686d9d4bffb21d579
Malcolm Smith <smith@chaquo.com>
2025-08-22 17:25:31 +0100

    Search in both the environment's bin directory and the PATH

--- cibuildwheel/platforms/android.py
+++ cibuildwheel/platforms/android.py
@@ -508,21 +508,28 @@ def repair_default(
         new_soname = soname_with_hash(src_path)
         dst_path = libs_dir / new_soname
         shutil.copyfile(src_path, dst_path)
-
-        # If cibuildwheel was called without activating its environment, the `bin`
-        # directory will not be on the PATH.
-        cibw_bin = Path(sys.executable).parent
-        call(cibw_bin / "patchelf", "--set-soname", new_soname, dst_path)
+        call(which("patchelf"), "--set-soname", new_soname, dst_path)
 
         for path in paths_to_patch:
-            call(cibw_bin / "patchelf", "--replace-needed", old_soname, new_soname, path)
+            call(which("patchelf"), "--replace-needed", old_soname, new_soname, path)
             call(
-                cibw_bin / "patchelf",
+                which("patchelf"),
                 "--set-rpath",
                 f"${{ORIGIN}}/{relpath(libs_dir, path.parent)}",
                 path,
             )
-        call(cibw_bin / "wheel", "pack", unpacked_dir, "-d", repaired_wheel_dir)
+        call(which("wheel"), "pack", unpacked_dir, "-d", repaired_wheel_dir)
+
+
+# If cibuildwheel was called without activating its environment, the `bin` directory
+# will not be on the PATH.
+def which(cmd: str) -> str:
+    cibw_bin = Path(sys.executable).parent
+    result = shutil.which(cmd, path=f"{cibw_bin}{os.pathsep}{os.environ['PATH']}")
+    if result is None:
+        msg = f"Couldn't find {cmd!r} in {cibw_bin} or on the PATH"
+        raise errors.FatalError(msg)
+    return result
 
 
 def elf_file_filter(paths: Iterable[Path]) -> Iterator[tuple[Path, ELFFile]]:
