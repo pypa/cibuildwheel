d07e708aedc1824d9699b681b6f0b7d85fd13bbe
Henry Schreiner <HenrySchreinerIII@gmail.com>
2026-02-10 14:33:21 -0500

    Apply suggestion from @henryiii

--- cibuildwheel/platforms/android.py
+++ cibuildwheel/platforms/android.py
@@ -697,7 +697,7 @@ def test_wheel(state: BuildState, wheel: Path, *, build_frontend: str) -> None:
 
     # Parse test-command.
     test_args = shlex.split(test_command)
-    if test_args[0] in {"python", "python3"} and any(arg in test_args for arg in {"-c", "-m"]}:
+    if test_args[0] in {"python", "python3"} and any(arg in test_args for arg in {"-c", "-m"}):
         # Forward the args to the CPython testbed script. We require '-c' or '-m'
         # to be in the command, because without those flags, the testbed script
         # will prepend '-m test', which will run Python's own test suite.
