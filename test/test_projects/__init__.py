5f85b96423be9d4f5f9cf981f39839497ad88ffa
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-05-13 15:52:27 +0530

    Add tests for `missing_dll_project`

--- test/test_projects/__init__.py
+++ test/test_projects/__init__.py
@@ -1,5 +1,5 @@
 from .base import TestProject
 from .meson import new_meson_project
-from .setuptools import new_c_project
+from .setuptools import new_c_project, new_c_project_with_missing_dll
 
-__all__ = ("TestProject", "new_c_project", "new_meson_project")
+__all__ = ("TestProject", "new_c_project", "new_c_project_with_missing_dll", "new_meson_project")
