89fa207e16d55bb42fec9c57bcd5a239f162c46a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-06-13 06:48:30 +0530

    Add test for pyodide-build as Pyodide default

--- unit_test/options_test.py
+++ unit_test/options_test.py
@@ -388,6 +388,19 @@ def test_build_frontend_option(
         assert parsed_build_frontend.args == ()
 
 
+def test_pyodide_build_frontend_default(tmp_path: Path) -> None:
+    args = CommandLineArguments.defaults()
+    args.package_dir = tmp_path
+
+    tmp_path.joinpath("pyproject.toml").write_text("[tool.cibuildwheel]\n")
+
+    options = Options(platform="pyodide", command_line_arguments=args, env={})
+    build_frontend = options.build_options(identifier=None).build_frontend
+
+    assert build_frontend.name == "pyodide-build"
+    assert build_frontend.args == ()
+
+
 def test_override_inherit_environment(tmp_path: Path) -> None:
     args = CommandLineArguments.defaults()
     args.package_dir = tmp_path
