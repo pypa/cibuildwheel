6e83a4668120ed86b28a8f1a24de1040f844c1ab
Malcolm Smith <smith@chaquo.com>
2026-08-08 10:13:02 +0100

    Update Android Python versions

--- cibuildwheel/platforms/android.py
+++ cibuildwheel/platforms/android.py
@@ -222,12 +222,6 @@ def setup_target_python(config: PythonConfiguration, build_path: Path) -> Path:
     python_dir = build_path / "python"
     python_dir.mkdir()
     shutil.unpack_archive(python_tgz, python_dir)
-
-    # Work around https://github.com/python/cpython/issues/138800. This can be removed
-    # once we've updated to Python versions that include the fix.
-    pc_path = python_dir / f"prefix/lib/pkgconfig/python-{config.version}.pc"
-    pc_path.write_text(pc_path.read_text().replace("$(BLDLIBRARY)", f"-lpython{config.version}"))
-
     return python_dir
 
 
